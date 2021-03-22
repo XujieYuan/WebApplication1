118847f17ab0414594d071f1b8ced99dd0bb4bd7
Xujie Yuan <xyua0019@student.monash.edu>
2021-03-22 12:40:19 +0800

    3.22 test

---
+++ SQLQuery1.sql
@@ -0,0 +1,52 @@
+﻿-- Creating table 'Users'
+CREATE TABLE [dbo].[Users] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [First_name] nvarchar(max)  NOT NULL,
+    [Last_name] nvarchar(max)  NOT NULL,
+    [Address] nvarchar(max)  NOT NULL
+);
+GO
+
+-- Creating table 'Bookings'
+CREATE TABLE [dbo].[Bookings] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [Email] nvarchar(max)  NOT NULL,
+    [Time] datetime  NOT NULL,
+    [UserId] int  NOT NULL
+);
+GO
+
+-- --------------------------------------------------
+-- Creating all PRIMARY KEY constraints
+-- --------------------------------------------------
+
+-- Creating primary key on [Id] in table 'Users'
+ALTER TABLE [dbo].[Users]
+ADD CONSTRAINT [PK_Users]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'Bookings'
+ALTER TABLE [dbo].[Bookings]
+ADD CONSTRAINT [PK_Bookings]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- --------------------------------------------------
+-- Creating all FOREIGN KEY constraints
+-- --------------------------------------------------
+
+-- Creating foreign key on [UserId] in table 'Bookings'
+ALTER TABLE [dbo].[Bookings]
+ADD CONSTRAINT [FK_UserBooking]
+    FOREIGN KEY ([UserId])
+    REFERENCES [dbo].[Users]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_UserBooking'
+CREATE INDEX [IX_FK_UserBooking]
+ON [dbo].[Bookings]
+    ([UserId]);
+GO
