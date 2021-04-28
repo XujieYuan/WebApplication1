0661875a2eb9eb868fd77999b7eccd84b8a9eb16
Xujie Yuan <xyua0019@student.monash.edu>
2021-04-28 11:53:56 +0800

    add game 1st step

---
+++ WebApplication1/Controllers/LoadHtmlController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class LoadHtmlController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [ChildActionOnly]
+        public ActionResult GetHtmlPage(string path)
+        {
+            return new FilePathResult(path, "text/html");
+        }
+    }
+}
